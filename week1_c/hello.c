3064a49c9dcc79db5c30886488e864ad366ea679
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-02 22:19:21 +0000

    Wed, Aug 2, 2023, 6:19 PM -04:00

--- week1_c/hello.c
+++ week1_c/hello.c
@@ -2,5 +2,9 @@
 
 int main(void)
 {
-    printf("Hello, world\n");
+    string first = get_string("What is your first name? ");
+    string last = get_string("What is your last name? ");
+    printf("Hello, %s %s\n", first, last);
 }
+
+#include <cs50.h>
