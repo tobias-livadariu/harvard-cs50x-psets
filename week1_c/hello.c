0a55f9798da90a6cb6ece6a4b62672d662932b65
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-02 22:21:32 +0000

    Wed, Aug 2, 2023, 6:21 PM -04:00

--- week1_c/hello.c
+++ week1_c/hello.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    float first = get_string("What is your first name? ");
+    string first = get_string("What is your first name? ");
     string last = get_string("What is your last name? ");
     printf("Hello, %s %s\n", first, last);
 }
