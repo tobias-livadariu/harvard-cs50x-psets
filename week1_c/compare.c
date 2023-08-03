dbcf3bed4d1cbb7175940b6209427dbc2003aef9
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-03 20:34:09 +0000

    Thu, Aug 3, 2023, 4:34 PM -04:00

--- week1_c/compare.c
+++ week1_c/compare.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    
+    int int1 = get_int("What is the value of integer 1? ");
+    int int2 = get_int("What is the value of integer 2? ");
+    if (int1 > int2)
+    {
+        printf("int1 (value of %s) is greater than int2 (value of %s)", int1, int2);
+    }
+    else if (int1 < int2)
+    {
+        printf("int1 (value of %s) is less than int2 (value of %s)", int1, int2);
+    }
+    else
+    {
+        printf("int1 (value of %s) is equal to int2 (value of %s)", int1, int2);
+    }
 }
