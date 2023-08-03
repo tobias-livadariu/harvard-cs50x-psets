1f828d6f004f3d14d0bdb7189f3b0a24af2aef37
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-03 21:01:38 +0000

    Thu, Aug 3, 2023, 5:01 PM -04:00

--- week1_c/agreement.c
+++ week1_c/agreement.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
 
 int main(void)
 {
-    string userRes = ""
-    while (userRes != )
+    char userRes = '';
+    while (userRes != 'y' && userRes != 'n')
+    {
+        printf("TERMS AND AGREEMENTS:\n");
+        printf("-------------------------------------\n");
+        printf("1. No anime\n");
+        printf("\n");
+        userRes = tolower(get_char("DO YOU ACCEPT THE FOLLOWING TERMS AND AGREEMENTS? (Y/N) "));
+        if (userRes == 'y')
+        {
+            printf("YOU HAVE ACCEPTED THE TERMS AND AGREEMENTS");
+        }
+        else if (userRes == 'n')
+        {
+            printf("YOU HAVE REJECTED THE TERMS AND AGREEMENTS");
+        }
+        else
+        {
+            printf("You entered this character: %c\nThis is invalid input, you must enter a Y or N\n", userRes);
+        }
+    }
 }
