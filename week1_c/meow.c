e974e2f862c020a82b07b3a69304123c6f9e66da
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-03 22:05:50 +0000

    Thu, Aug 3, 2023, 6:05 PM -04:00

--- week1_c/meow.c
+++ week1_c/meow.c
@@ -3,5 +3,13 @@
 
 int main(void)
 {
-    
+    int numMeows = 0;
+    while (numMeows < 1)
+    {
+        numMeows = get_int("How many meows would you like? (enter a positive integer) ");
+        if (numMeows < 1)
+        {
+            printf("You entered %d")
+        }
+    }
 }
