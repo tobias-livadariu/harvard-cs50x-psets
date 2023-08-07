b99a217052e178937441d2644d13d944960b5015
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 02:01:18 +0000

    Sun, Aug 6, 2023, 10:01 PM -04:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,17 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    popStartSize
+    int popStartSize = get_int("Start size: ");
+
     // TODO: Prompt for end size
+    int popEndSize = get_int("End size: ");
 
     // TODO: Calculate number of years until we reach threshold
+    int numYears = 0;
+    while (popEndSize >= popStartSize);
+    {
+        popStartSize = popStartSize + (popStartSize / 3) - (popStartSize / 4)
+    }
 
     // TODO: Print number of years
 }
