b8c903a824c84f1470832934ea44375294c21f8b
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 20:32:11 +0000

    Mon, Aug 7, 2023, 4:32 PM -04:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-    // Initializing a variable called name that takes string values and assigning to it the user's input when prompted to enter their name
+    /* Initializing a variable called name that takes string values
+    and assigning to it the user's input when prompted to enter their name */
     string name = get_string("What's your name? ");
     // Printing the user's name to the screen in a more personalized version of "hello, world"
     printf("hello, %s\n", name);
