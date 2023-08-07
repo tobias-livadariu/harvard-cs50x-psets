13e1508cb8bb6a56322f1e20d9f14758d9f402bb
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 21:02:53 +0000

    Mon, Aug 7, 2023, 5:02 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,18 +3,23 @@
 
 // Referencing the function that is used to print the vertical columns
 void column_printer(int size);
-// Referencing the function that can be used to 
+// Referencing the function that can be used to repeatedly print white spaces
 void space_printer(int length);
 
 int main(void)
 {
+    // Initializing the variable that will store the height of the pyramid.
     int height;
+    /* Using a do-while loop to prompt the user for the integer height of the pyramid,
+    and reprompting them if the height isn't between 1 and 8 inclusively (note that I learned to do multi-line comments in C from my dad). */
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
 
+    /* Using a for loop to print out the pyramid.
+    Each iteration of this for loop prints out one row of the pyramid. */
     for (int row = 0; row < height; row++)
     {
         space_printer(height - (row + 1));
