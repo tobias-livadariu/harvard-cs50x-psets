ba42b6f1ba300a00cc341b955c2cc407783eae2a
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 20:48:05 +0000

    Mon, Aug 7, 2023, 4:48 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void column_printer(int size);
+
 int main(void)
 {
     int height;
@@ -8,5 +10,21 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while (height )
+    while (height < 1 || height > 8);
+
+    for (int row = 0; row < height; row++)
+    {
+        column_printer(row);
+        printf(" ");
+        column_printer(row);
+        printf("\n");
+    }
+}
+
+void column_printer(int size)
+{
+    for (int column = 0; column <= size; column++)
+        {
+            printf("#");
+        }
 }
