1c69483834474b2115d0e0110176d3c9019412c0
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 20:56:01 +0000

    Mon, Aug 7, 2023, 4:56 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Referencing the function that is used to print the vertical columns
 void column_printer(int size);
+// Referencing the function that can be used to 
 void space_printer(int length);
 
 int main(void)
@@ -23,6 +25,7 @@ int main(void)
     }
 }
 
+// Defining the function that is used to print the vertical columns
 void column_printer(int size)
 {
     for (int column = 0; column <= size; column++)
@@ -31,6 +34,7 @@ void column_printer(int size)
         }
 }
 
+// Defining the function that is used to print spaces to properly space out the vertical columns of increasing and then decreasing height
 void space_printer(int length)
 {
     for (int count = 0; count < length; count++)
--- week1_c/hello.c
+++ week1_c/hello.c
@@ -5,5 +5,5 @@ int main(void)
 {
     string first = get_string("What is your first name? ");
     string last = get_string("What is your last name? ");
-    printf("Hello, %s %s\n", first, last);
+    printf("Hello, %s %s\n" * 3, first, last);
 }
