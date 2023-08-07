48eeaf82fb03943b2d3e094b984e21edb27db99c
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 21:42:52 +0000

    Mon, Aug 7, 2023, 5:42 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int exponentation(int num, int pow)
+
 int main(void)
 {
     // Getting the user's credit card number as a long
@@ -20,15 +22,28 @@ int main(void)
     int curDigit;
     for (digitCount = 0; digitCount < numLength; digitCount++)
     {
-        curDigit = userNumber % (10 )
+        if (digitCount % 2 == 1)
+        {
+
+        }
+        else
+        {
+            if (digitCount == 0)
+            {
+                curDigit = 
+            }
+        }
     }
 }
 
+// Defining a function that can be used to raise numbers to exponents
+// This function will be used to generate powers of ten for getting individual digits of the user's number
 int exponentation(int num, int pow)
 {
-    int exponentedNum = 0;
+    int exponentedNum = 1;
     for (count = 0; count < pow; count++)
     {
-        exponentedNum
+        exponentedNum *= num;
     }
+    return exponentedNum
 }
