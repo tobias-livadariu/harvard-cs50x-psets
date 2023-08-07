01d2e55302dd0ec9346e5dd9d337dcc1606361cf
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 20:39:32 +0000

    Mon, Aug 7, 2023, 4:39 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height )
 }
