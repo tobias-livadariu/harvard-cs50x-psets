791e177605e98be0b83a8a06ad320dce4d35c559
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-07 21:31:54 +0000

    Mon, Aug 7, 2023, 5:31 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,7 +3,22 @@
 
 int main(void)
 {
-    userNumber = get_long("Number: ")
+    // Getting the user's credit card number as a long
+    long userNumber = get_long("Number: ");
 
-    
+    // Determining the number of digits in the user's credit card number
+    long storageVar = userNumber;
+    int numLength = 0;
+    while (! (storageVar = 0))
+    {
+        storageVar /= 10;
+        numLength += 1;
+    }
+
+    // Determining if the user's inputted number passes the chucksum as described by Luhan's Algorithm
+    int checksum = 0;
+    for (digit = 0; digit < numLength; digit++)
+    {
+        
+    }
 }
