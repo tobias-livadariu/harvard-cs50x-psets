ae4065f1f85c965c52b9cad3e40211943c11451a
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-08 00:22:20 +0000

    Mon, Aug 7, 2023, 8:22 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -25,6 +25,11 @@ int main(void)
         if (digitCount % 2 == 0)
         {
             curDigit = (userNumber % exponentation(10, digitCount)) - (userNumber % exponentation(10, digitCount - 1));
+            if (curDigit > 4)
+            {
+                curDigit *= 2
+                
+            }
         }
         else
         {
