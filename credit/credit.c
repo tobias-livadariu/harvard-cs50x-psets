e752e8b0434caf29dadb91e8c27ffe655d88f5b2
Basasel <76631595+Basasel@users.noreply.github.com>
2023-08-09 19:24:51 +0000

    Wed, Aug 9, 2023, 3:24 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int exponentation(int num, int pow)
+int exponentation(int num, int pow);
 
 int main(void)
 {
@@ -26,7 +26,7 @@ int main(void)
     int tensDigit;
     int onesDigit;
     int curDigit;
-    for (digitCount = 1; digitCount <= numLength; digitCount++)
+    for (int digitCount = 1; digitCount <= numLength; digitCount++)
     {
         if (digitCount % 2 == 0)
         {
@@ -60,22 +60,25 @@ int main(void)
         }
     }
 
-    // If the user's credit card number passes
+    // If the user's credit card number passes Luhan's Algorithm, determining if it falls into the AMEX/MASTERCARD/VISA category or is INVALID regardless
     if (checksum % 10 == 0)
     {
-        if (numLength == 15 && (firstTwo == ))
+        if (numLength == 15 && (firstTwo == 34 || firstTwo == 37))
         {
-
+            printf("AMEX\n");
         }
-        else if (numLength == 16 && )
+        else if (numLength == 16 && (firstTwo == 51 || firstTwo == 52 || firstTwo == 53 || firstTwo == 54 || firstTwo == 55))
         {
-
+            printf("MASTERCARED\n");
         }
-        else if ((numLength == 13 || numLength == 16) && )
+        else if ((numLength == 13 || numLength == 16) && (firstTwo % 10) / 10 == 4)
+        {
+            printf("VISA\n");
+        }
     }
     else
     {
-        printf("INVALID")
+        printf("INVALID\n");
     }
 }
 
@@ -84,9 +87,9 @@ int main(void)
 int exponentation(int num, int pow)
 {
     int exponentedNum = 1;
-    for (count = 0; count < pow; count++)
+    for (int count = 0; count < pow; count++)
     {
         exponentedNum *= num;
     }
-    return exponentedNum
+    return exponentedNum;
 }
