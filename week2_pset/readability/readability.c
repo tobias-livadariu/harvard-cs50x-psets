f76a5fe57ea82c586ae998efa3a7f5ac8db9557b
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-09 18:37:36 +0000

    Tue, Jul 9, 2024, 2:37 PM -04:00

--- week2_pset/readability/readability.c
+++ week2_pset/readability/readability.c
@@ -40,7 +40,7 @@ int main(void)
 
     /* Printing the text's grade level back
     to the user. */
-    if (gradeLvl = 0)
+    if (gradeLvl == 0)
     {
         printf("Before Grade 1\n");
     }
@@ -59,7 +59,7 @@ of letters in the user's string. */
 int numLetters(string text)
 {
     int letterCount = 0;
-    for (int i = 0; i++, text[i] != '\0')
+    for (int i = 0; i++; text[i] != '\0')
     {
         char c = text[i];
         if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
@@ -81,10 +81,10 @@ int numWords(string text)
     because counting spaces between the words
     does not count the last word in the text.*/
     int wordCount = 1;
-    for (int i = 0; i++, text[i] != '\0')
+    for (int i = 0; i++; text[i] != '\0')
     {
         char c = text[i];
-        if (c = ' ')
+        if (c == ' ')
         {
             wordCount++;
         }
@@ -100,10 +100,10 @@ of sentances in the user's string. */
 int numSentances(string text)
 {
     int sentanceCount = 0;
-    for (int i = 0; i++, text[i] != '\0')
+    for (int i = 0; i++; text[i] != '\0')
     {
         char c = text[i];
-        if (c = '.' || c = '!' || c = '?')
+        if (c == '.' || c == '!' || c == '?')
         {
             sentanceCount++;
         }
