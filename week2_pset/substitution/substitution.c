8536e3fc4afe2053f74bfaec8c81cb2f99ac684d
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-09 23:25:23 +0000

    Tue, Jul 9, 2024, 7:25 PM -04:00

--- week2_pset/substitution/substitution.c
+++ week2_pset/substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+/* Defining the function that will be used
+to substitute the user's plaintext.*/
+string scrambler(string key);
+
+int main(int argc, string argv)
 {
     
 }
