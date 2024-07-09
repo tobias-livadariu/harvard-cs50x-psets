d077f291540378a78daf59e6af5dedb0d4de92e5
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-09 18:10:12 +0000

    Tue, Jul 9, 2024, 2:10 PM -04:00

--- week2_pset/readability/readability.c
+++ week2_pset/readability/readability.c
@@ -3,11 +3,23 @@
 
 /* Prototype declarations for the functions
 used in this program. */
-int numWords(string text);
 int numLetters(string text);
+int numWords(string text);
 int numSentances(string text);
 
 int main(void)
 {
-    
+    // Getting a string from the user.
+    string userText = get_string("Text: ");
+
+    /* Using the prototyped functions above to find the number of words,
+    letters, and sentances in the user's string. */
+    int numUserLetters = numLetters(userText);
+    int numUserWords = numWords(userText);
+    int numUserSentances = numSentances(userText);
+
+    /* Finding the values of L and S in the user's string
+    for the Coleman-Lau index. */
+    L = () / ()
 }
+
