30f69b3aaae889645496c8edd5b18023fb540e5c
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-16 01:32:18 +0000

    Mon, Jul 15, 2024, 9:32 PM -04:00

---
+++ week3_pset/plurality/plurality.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+
+int main(int argc, string argv[])
+{
+    if (argc < 1)
+    {
+        printf("Usage: ./plurality [candidate ...]\n");
+        return 1;
+    }
+    else
+    {
+        int numVoters = get_int("Number of voters: ");
+    }
+}
