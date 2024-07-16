0b2bf087bb6165caaa487e2bffab24183cca399d
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-16 01:37:20 +0000

    Mon, Jul 15, 2024, 9:37 PM -04:00

--- week3_pset/plurality/plurality.c
+++ week3_pset/plurality/plurality.c
@@ -3,16 +3,41 @@
 #include <ctype.h>
 #include <string.h>
 
+/* Defining the datatype structure that will
+be used to store each candidate and their number of
+votes. */
+typedef struct
+{
+    string name;
+    int votes;
+}candidate;
 
 int main(int argc, string argv[])
 {
     if (argc < 1)
     {
+        /* Warning the user for incorrect
+        input. */
         printf("Usage: ./plurality [candidate ...]\n");
         return 1;
     }
     else
     {
+        /* Getting the number of voters
+        from the user. */
         int numVoters = get_int("Number of voters: ");
     }
+
+    /* Defining the array that will
+    store the candidates and their number of votes. */
+    candidate runners[numVoters];
+
+    /* Assigning the runners inputted
+    into the program to the runners[] array. */
+    for (int i = 0; i < numVoters; i++)
+    {
+        runners[i].name = argv[i];
+    }
+
+    
 }
