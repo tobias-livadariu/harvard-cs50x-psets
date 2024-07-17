8f6b0545e91967390a066adc50a807a4e2792375
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-17 23:47:59 +0000

    Wed, Jul 17, 2024, 7:47 PM -04:00

--- week3_pset/plurality/plurality.c
+++ week3_pset/plurality/plurality.c
@@ -65,11 +65,26 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
+    int numFails = 0;
     for (int i = 0; i < (argc - 1); i++)
     {
-        if (strcmp(candidates[i].name, ))
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+        }
+        else
+        {
+            numFails++;
+        }
     }
-    return false;
+    if (numFails == (argc - 1))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 // Print the winner (or winners) of the election
