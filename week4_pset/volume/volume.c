967818c42cc4c5a2c6afe2e17cf0fa2becf6f409
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-21 04:20:58 +0000

    Sun, Jul 21, 2024, 12:20 AM -04:00

--- week4_pset/volume/volume.c
+++ week4_pset/volume/volume.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     BYTE *singleRead = malloc(sizeof(BYTE) * 2);
     while (fread(singleRead, sizeof(BYTE) * 2, 1, input) != 0)
     {
-        singleRead *= factor;
+        *singleRead *= factor;
         fwrite(singleRead, sizeof(BYTE) * 2, 1, output);
     }
     free (singleRead);
