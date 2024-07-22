f58ccf91d7ea8d55514906c6292f548f06cef428
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-22 04:05:07 +0000

    Mon, Jul 22, 2024, 12:05 AM -04:00

--- week4_pset/filter-more/helpers.c
+++ week4_pset/filter-more/helpers.c
@@ -55,7 +55,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            if (i == 0)
+            {
+                if (j == 0)
+                {
+                    int blurredPixel = ()
+                }
+            }
         }
     }
 }
