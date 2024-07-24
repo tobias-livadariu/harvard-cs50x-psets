d1155caf1ed6211b9456e39bd21685636af04bc9
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-24 04:30:50 +0000

    Wed, Jul 24, 2024, 12:30 AM -04:00

--- week4_pset/recover/recover.c
+++ week4_pset/recover/recover.c
@@ -3,5 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-    
+    /* Checking if the user inputted
+    the correct number of command-line arguments. */
+    if (argc != 2)
+    {
+        printf("Usage: ./recover [forensic image]\n
+        --> note that exactly one command-line argument must be used\n");
+        return 1;
+    }
+    /* Opening the forensic image for reading,
+    and warning the user if it was unable to
+    be opened.*/
+    FILE *forensicImage = openf(argv[1], "r");
+    if (forensicImage == NULL)
+    {
+        printf("Unable to open forensic image for reading!\n
+        Check the inputted forensic image address for errors.\n");
+        return 1;
+    }
+    /*  */
+    int numFilesCopied = 0;
+
 }
