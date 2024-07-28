bd9ecd9459e51281278457995395792baf51af9c
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-07-28 01:55:40 +0000

    Sat, Jul 27, 2024, 9:55 PM -04:00

--- week5_pset/inheritance/inheritance_vI_Have_No_Idea_What_I_Am_Doing.c
+++ week5_pset/inheritance/inheritance_vI_Have_No_Idea_What_I_Am_Doing.c
@@ -49,28 +49,28 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person.parents[0] = parent0;
-        new_person.parents[1] = parent1;
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person.alleles[0] = new_person.parents[0]->alleles[rand() % 2];
-        new_person.alleles[1] = new_person.parents[0]->alleles[rand() % 2];
+        new_person->alleles[0] = new_person->parents[0]->alleles[rand() % 2];
+        new_person->alleles[1] = new_person->parents[0]->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        new_person.parents[0] = NULL;
-        new_person.parents[1] = NULL;
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        new_person.alleles[0] = random_allele();
-        new_person.alleles[1] = random_allele();
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return *new_person;
+    return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
@@ -79,7 +79,7 @@ void free_family(person *p)
     // TODO: Handle base case
     if (p == NULL)
     {
-        return 0;
+        return;
     }
 
     // TODO: Free parents recursively
