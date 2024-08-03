019ebec7a353afd5bbde136ccc8c7413935ca479
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-03 06:26:19 +0000

    Sat, Aug 3, 2024, 2:26 AM -04:00

--- week6_pset/sentimental-credit/credit.py
+++ week6_pset/sentimental-credit/credit.py
@@ -2,10 +2,15 @@
 # be used to determine if an inputted
 # credit card is valid
 def isValid(number)
+    # Calculating the digit sum.
     strNumber = str(number)
+    strNumber = strNumber[::-1]
     digitSum = 0
     for i in range(0, len(strNumber), 2):
         digitSum += int(strNumber[i])
     for i in range(1, len(strNumber), 2):
         digitSum += (int(strNumber[i] * 2))
+
+    # Finding if the digit sum is valid.
+    if digitSum % 0 ==  
 
