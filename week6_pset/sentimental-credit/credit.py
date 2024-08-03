56726b334bd14672065dea294f5f6e07589cbc76
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-03 07:03:43 +0000

    Sat, Aug 3, 2024, 3:03 AM -04:00

--- week6_pset/sentimental-credit/credit.py
+++ week6_pset/sentimental-credit/credit.py
@@ -11,7 +11,7 @@ def isValid(number):
     for i in range(0, len(strNumber), 2):
         digitSum += int(strNumber[i])
     for i in range(1, len(strNumber), 2):
-        digitSum += (int(strNumber[i] * 2))
+        digitSum += (int(strNumber[i]) * 2)
 
     # Finding if the digit sum is valid.
     if digitSum % 10 == 0:
