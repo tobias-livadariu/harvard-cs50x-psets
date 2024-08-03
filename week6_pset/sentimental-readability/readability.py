daaaa16c7f70c894fd0c1238e24a1ca7413cdcc9
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-03 07:37:55 +0000

    Sat, Aug 3, 2024, 3:37 AM -04:00

--- week6_pset/sentimental-readability/readability.py
+++ week6_pset/sentimental-readability/readability.py
@@ -17,3 +17,23 @@ for c in userStr:
         numSentances += 1
     elif c == " ":
         numWords += 1
+
+L = numLetters * (100 / numWords)
+S = numSentances * (100 / numWords)
+
+# Calculating the colemen-liau index.
+index = 0.0588 * L - 0.296 * S - 15.8
+
+# Rounding the index.
+decimal = index - int(index)
+if (decimal >= 0.5):
+    index = int(index) + 1
+else:
+    index = int(index)
+
+if index < 1:
+    print("Before Grade 1")
+elif index > 15:
+    print("Grade 16+")
+else:
+    print(f"Grade {index}")
