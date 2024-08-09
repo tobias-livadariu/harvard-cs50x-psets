2fb2470498ca6d7cbf78cb2844cdd2e435c35891
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-09 20:30:45 +0000

    Fri, Aug 9, 2024, 4:30 PM -04:00

--- week7_pset/fiftyville/log_v2.sql
+++ week7_pset/fiftyville/log_v2.sql
@@ -7,7 +7,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Querying the crime_scene_reports table for information on the crime.
-SELECT description, street FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND s.treet = "Humphrey Street";
+SELECT description, street FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 -- Duck theft crime description:
 -- *Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
 -- *Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
