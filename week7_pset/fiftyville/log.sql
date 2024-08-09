315263570dc9891d7dd065c83de39c5942f90562
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-09 00:36:27 +0000

    Thu, Aug 8, 2024, 8:36 PM -04:00

--- week7_pset/fiftyville/log.sql
+++ week7_pset/fiftyville/log.sql
@@ -93,3 +93,16 @@ SELECT license_plate FROM bakery_security_logs WHERE year = 2023 AND month = 7 A
 -- Querying for the transcripts and names of the interviewed people for all interviews
 -- that were taken on the day that the duck was stolen.
 SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
+-- Here are all the transcripts relating to the stolen duck:
+/* Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
+If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame. */
+/* I don't know the thief's name, but it was someone I recognized.
+Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money. */
+/* As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
+In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
+The thief then asked the person on the other end of the phone to purchase the flight ticket. */
+
+-- From this, I learned that the theif left the bakery within ten minutes after the duck was stolen, so from minutes of 15 - 26
+-- (adding one extra minute to be safe).
+
+-- Also, the theif left the 
