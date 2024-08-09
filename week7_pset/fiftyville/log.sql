6524f9fa20e4edde919af2f269a71cb54b52e0ec
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-09 00:43:02 +0000

    Thu, Aug 8, 2024, 8:43 PM -04:00

--- week7_pset/fiftyville/log.sql
+++ week7_pset/fiftyville/log.sql
@@ -113,6 +113,5 @@ The thief then asked the person on the other end of the phone to purchase the fl
 
 -- To find all possible liscence plates of the theif, I will have to further analyze the bakery's
 -- security logs and liscence plate logs for all cars leaving the premises from minutes of 15 - 26.
--- Note that I already queried for 16 and found a possible liscence plate.
-SELECT activity FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute = 17;
-SELECT license_plate FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute = 17;
+-- Note that I already queried for 16 minutes, but I will include that result here as well for convinence.
+SELECT activity FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 26;
