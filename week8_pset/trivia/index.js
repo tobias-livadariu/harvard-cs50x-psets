e20fe17b18e8d7b8dfb509c58ade9923fb1e846c
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-10 00:14:44 +0000

    Fri, Aug 9, 2024, 8:14 PM -04:00

--- week8_pset/trivia/index.js
+++ week8_pset/trivia/index.js
@@ -1,12 +1,22 @@
 let buttons = document.querySelectorAll('button');
 let form = document.querySelector('form');
 
-function incorrectButtonClick(id)
+function buttonClick(id)
 {
-
+    if (id == '#rightAnswer')
+    {
+        let paragraph1 = document.querySelector('#part1Paragraph');
+        let button = document.querySelector(id);
+        paragraph1.textContent = "Correct!";
+        paragraph1.style.color = 'green';
+    }
+    else
+    {
+        let paragraph1 = document.querySelector('#part1Paragraph');
+        let button = document.querySelector(id);
+        paragraph1.textContent = "Incorrect";
+        paragraph1.style.color = 'red';
+    }
 }
 
-function correctButtonClick(id)
-{
-
-}
+function
