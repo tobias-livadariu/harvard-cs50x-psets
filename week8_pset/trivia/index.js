1f45c1ce8834efe27a7bb6e7582a1837d56a7b1e
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-10 00:05:47 +0000

    Fri, Aug 9, 2024, 8:05 PM -04:00

--- week8_pset/trivia/index.js
+++ week8_pset/trivia/index.js
@@ -1,8 +1,7 @@
-let button1 = document.querySelector('#right_answer');
-let button2 = document.querySelector('#wrong_answer_1');
-let button3 = document.querySelector('#wrong_answer_2');
-let button4 = document.querySelector('#wrong_answer_3');
-let button5 = document.querySelector('#wrong_answer_4');
+let buttons = document.querySelectorAll('button');
 let form = document.querySelector('form');
 
 function incorrectButtonClick(id)
+{
+    
+}
