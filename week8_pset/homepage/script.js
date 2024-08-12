ac46363b8d2e67aa7db2cf88dc63e97b9bd0b06c
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-12 16:54:17 +0000

    Mon, Aug 12, 2024, 12:54 PM -04:00

--- week8_pset/homepage/script.js
+++ week8_pset/homepage/script.js
@@ -4,6 +4,12 @@ let imageChosen = 0;
 /* Defining the imageSwitcher function. */
 function imageSwitcher()
 {
+    /* Defining a variable through which the dog image can be accessed. */
     let dogImage = document.getElementById('dog_image');
-    
+
+    /* Randomizing the imageChosen value to be between 1 and 5. */
+    imageChosen = Math.floor(Math.random() * 5) + 1;
+
+    /* Defining a variable to hold the new image address string. */
+    let imageAddress = 
 }
