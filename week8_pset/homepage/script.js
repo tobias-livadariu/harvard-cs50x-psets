2c8a98e555eab8a032621ea9647364612950f4d1
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-12 16:51:03 +0000

    Mon, Aug 12, 2024, 12:51 PM -04:00

---
+++ week8_pset/homepage/script.js
@@ -0,0 +1,9 @@
+/* Setting the imageChosen variable to 0. */
+let imageChosen = 0;
+
+/* Defining the imageSwitcher function. */
+function imageSwitcher()
+{
+    let dogImage = document.getElementById('dog_image');
+    
+}
