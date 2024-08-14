2ad43598ea80c49ec266001c1894eabea9a8d619
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-14 16:55:25 +0000

    Wed, Aug 14, 2024, 12:55 PM -04:00

--- week9_pset/birthdays/app.py
+++ week9_pset/birthdays/app.py
@@ -31,7 +31,7 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
         # Validating the user's input server-side.
-        if not name or not (1 <= month <= 12) or (not day):
+        if not name or not (1 <= month <= 12) or not (1 <= day <= 31):
             return redirect("/failure")
 
         # Adding the user's input to the database.
