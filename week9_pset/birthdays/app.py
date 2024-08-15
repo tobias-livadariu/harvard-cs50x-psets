75350b59a3b285a3541e6881fe2b4aa559729b80
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-15 03:08:42 +0000

    Wed, Aug 14, 2024, 11:08 PM -04:00

--- week9_pset/birthdays/app.py
+++ week9_pset/birthdays/app.py
@@ -48,11 +48,13 @@ def index():
 
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
-        return render_template("index.html", birthdays = birthdays)
+        return render_template("index.html", birthdays=birthdays)
+
 
 @app.route("/failure")
 def failure():
     return render_template("failure.html")
+
 
 @app.route("/remove", methods=["POST"])
 def remove():
