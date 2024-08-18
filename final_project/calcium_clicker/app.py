0f7d7af8111778bf2d92a55ffdf34d2623fffbe9
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-18 22:50:40 +0000

    Sun, Aug 18, 2024, 6:50 PM -04:00

--- final_project/calcium_clicker/app.py
+++ final_project/calcium_clicker/app.py
@@ -1,2 +1,4 @@
+# Importing external libraries and frameworks
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
