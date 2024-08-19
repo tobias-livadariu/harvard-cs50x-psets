99eabf842f9b088e5c816705d445a390e69d43a9
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-19 19:01:37 +0000

    Mon, Aug 19, 2024, 3:01 PM -04:00

--- final_project/calcium_clicker/app.py
+++ final_project/calcium_clicker/app.py
@@ -17,7 +17,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 # Configuring the flask application
 app = Flask(__name__)
