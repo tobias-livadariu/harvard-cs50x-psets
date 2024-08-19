4dc56588194ec4077cf19c82464d4edbcdb8e70d
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-19 16:56:39 +0000

    Mon, Aug 19, 2024, 12:56 PM -04:00

--- final_project/calcium_clicker/app.py
+++ final_project/calcium_clicker/app.py
@@ -1,6 +1,7 @@
-"""Note that the libraries imported from """
+"""Note that the libraries imported from lines 2 - 5 were taken
+from the Finance pset."""
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
--- final_project/calcium_clicker/helpers.py
+++ final_project/calcium_clicker/helpers.py
@@ -1,7 +1,12 @@
+"""It is important to note that both the apology and login_required
+functions were directly taken from the Finance pset's helpers.py file. To support these
+functions, the libraries used in Finance's helpers.py file were imported."""
+
 import csv
 import datetime
 import pytz
 import requests
+
 import urllib
 import uuid
 
@@ -48,41 +53,3 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
-
-
-def lookup(symbol):
-    """Look up quote for symbol."""
-
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
-
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
-
-    # Query API
-    try:
-        response = requests.get(
-            url,
-            cookies={"session": str(uuid.uuid4())},
-            headers={"Accept": "*/*", "User-Agent": request.headers.get("User-Agent")},
-        )
-        response.raise_for_status()
-
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        price = round(float(quotes[-1]["Adj Close"]), 2)
-        return {"price": price, "symbol": symbol}
-    except (KeyError, IndexError, requests.RequestException, ValueError):
-        return None
-
-
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
