43fccc51f07200d49f29d6a0bb5b1014793329f0
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-19 17:20:45 +0000

    Mon, Aug 19, 2024, 1:20 PM -04:00

--- final_project/calcium_clicker/app.py
+++ final_project/calcium_clicker/app.py
@@ -60,76 +60,7 @@ def after_request(response):
 def index():
     """TODO: make the main page"""
 
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """If the route was requested via post, buy shares of stock."""
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        shares = request.form.get("shares")
-
-        try:
-            shares = float(shares)
-        except ValueError:
-            return apology("You must return a numeric value for your number of shares!")
-
-        if not symbol:
-            return apology("You did not input a stock symbol! Next time, please input a valid stock symbol.")
-        if (shares - int(shares)) != 0 or shares < 1:
-            return apology("When choosing the number of shares you would like to purchase, please use a positive integer.")
-        shares = int(shares)
-        price = lookup(symbol)
-        if not price:
-            return apology("The stock symbol you inputted does not exist! Please try a different stock symbol.")
-
-        # Getting the user's cash balance and extracting it
-        userBalPacked = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
-        userBal = userBalPacked[0]["cash"]
-
-        totalCost = shares * price["price"]
-        if userBal < totalCost:
-            return apology(f"You cannot afford that transaction! Remember, your current balance is ${userBal:.2f} USD.")
-
-        # Getting the number of stocks currently owned by the user and extracting it
-        numStocksPacked = db.execute(
-            "SELECT stock_count FROM stocks WHERE stock_symbol = ? AND user_id = ?", symbol, session["user_id"])
-        if not numStocksPacked:
-            db.execute("INSERT INTO stocks (user_id, stock_symbol, stock_count) VALUES (?, ?, ?)",
-                       session["user_id"], symbol, shares)
-        else:
-            numStocks = numStocksPacked[0]["stock_count"]
-            db.execute("UPDATE stocks SET stock_count = stock_count + ? WHERE user_id = ? AND stock_symbol = ?",
-                       shares, session["user_id"], symbol)
-        db.execute("UPDATE users SET cash = ? WHERE id = ?",
-                   (userBal - totalCost), session["user_id"])
-
-        # Determining the date of transaction.
-        currentDate = datetime.now()
-        dateFormatted = currentDate.strftime("%Y/%m/%d")
-
-        # Inserting the completed transaction into the histories table.
-        db.execute("INSERT INTO histories (user_id, stock_symbol, transaction_price, stock_count, transaction_type, transaction_date) VALUES (?, ?, ?, ?, ?, ?)",
-                   session["user_id"], symbol, price["price"], shares, "Buy", dateFormatted)
-
-        # Redirecting the user.
-        return redirect("/")
-
-    # If the page was accessed via "get", rendering the buy.html template.
-    return render_template("buy.html")
-
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    histories = db.execute(
-        "SELECT stock_symbol, transaction_price, stock_count, transaction_type, transaction_date FROM histories WHERE user_id = ? ORDER BY transaction_date DESC", session["user_id"])
-    if not histories:
-        return render_template("empty_history.html")
-    return render_template("history.html", histories=histories)
-
-
+"""NOTE: the login route was taken from my work in Finance."""
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
@@ -168,7 +99,8 @@ def login():
     else:
         return render_template("login.html")
 
-
+"""NOTE: the logout route was taken from the distribution code
+in Finance."""
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -179,25 +111,7 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        if not symbol:
-            return apology("When getting a quote, you must input the symbol of the stock being quoted!")
-
-        quote = lookup(symbol)
-        if not quote:
-            return apology("The stock symbol you quoted does not exist! Please try a different stock symbol.")
-        return render_template("quoted.html", quote=quote)
-
-    """If the page was requested via get, rendering it."""
-    return render_template("quote.html")
-
-
+"""NOTE: the register route was taken from my work in Finance."""
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
@@ -240,57 +154,3 @@ def register():
 
     """If the register page was accessed via "get", displaying it to the user."""
     return render_template("register.html")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    # Getting all the shares that the user owns
-    stocks = db.execute("SELECT stock_symbol FROM stocks WHERE user_id = ?", session["user_id"])
-
-    """Sell shares of stock"""
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        shares = request.form.get("shares")
-
-        try:
-            shares = float(shares)
-        except ValueError:
-            return apology("You must return a numeric value for your number of shares!")
-
-        if not symbol:
-            return apology("You did not input a stock symbol! Next time, please input a valid stock symbol.")
-        if (shares - int(shares)) != 0 or shares < 1:
-            return apology("When choosing the number of shares you would like to purchase, please use a positive integer.")
-        shares = int(shares)
-        price = lookup(symbol)
-        if not price:
-            return apology("The stock symbol you inputted does not exist! Please try a different stock symbol.")
-
-        # checking if the symbol that the user inputted is a stock that they own.
-        userStock = db.execute(
-            "SELECT stock_count FROM stocks WHERE user_id = ? AND stock_symbol = ?", session["user_id"], symbol)
-        if not userStock or userStock[0]["stock_count"] < shares:
-            return apology("You either do not own this stock or do not have enough shares to sell.")
-
-        # Completing the sale.
-        totalSale = shares * price["price"]
-        db.execute("UPDATE stocks SET stock_count = stock_count - ? WHERE user_id = ? AND stock_symbol = ?",
-                   shares, session["user_id"], symbol)
-        db.execute("UPDATE users SET cash = cash + ? WHERE id = ?", totalSale, session["user_id"])
-
-        # Removing the entry from the stocks table if the user sold all of their shares.
-        db.execute("DELETE FROM stocks WHERE stock_count = 0 AND user_id = ? AND stock_symbol = ?",
-                   session["user_id"], symbol)
-
-        # Determining the date of transaction.
-        currentDate = datetime.now()
-        dateFormatted = currentDate.strftime("%Y/%m/%d")
-
-        # Inserting the completed transaction into the histories table.
-        db.execute("INSERT INTO histories (user_id, stock_symbol, transaction_price, stock_count, transaction_type, transaction_date) VALUES (?, ?, ?, ?, ?, ?)",
-                   session["user_id"], symbol, price["price"], shares, "Sell", dateFormatted)
-
-        # Redirecting the user back to the homepage.
-        return redirect("/")
-    return render_template("sell.html", stocks=stocks)
