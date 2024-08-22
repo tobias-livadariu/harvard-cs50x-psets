dcd95891cdd0a13cb19b11a85f73056770520f38
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-22 03:49:10 +0000

    Wed, Aug 21, 2024, 11:49 PM -04:00

--- final_project/calcium_clicker_v5/static/js/buyAutodigger.js
+++ final_project/calcium_clicker_v5/static/js/buyAutodigger.js
@@ -5,24 +5,8 @@ function buyShovel() {
     })
     .then(response => response.json())
     .then(data => {
-        let shovelButton = document.getElementById("shovel-button");
-        let curShovel = data.curShovel;
-        let maxShovel = data.maxShovel;
-        let shovels = data.shovels;
-        let shovelCost = data.shovelCost;
-
-        if (curShovel < maxShovel) {
-            shovelButton.textContent = `Forge a ${shovels[curShovel + 1]} shovel (cost: ${shovelCost} skeletons)`;
-            // Remove dark class and enable the button (this is not necessary, but I am writing it for clarity)
-            shovelButton.classList.remove('btn-dark');
-            shovelButton.removeAttribute('disabled');
-        } else {
-            // Change the text content in the button to indicate that it is maxed
-            shovelButton.textContent = "Your shovel is max level!";
-            // Add the dark class to the button and disable it
-            shovelButton.classList.add("btn-dark");
-            shovelButton.setAttribute("disabled", "true")
-        }
+        document.getElementById("num-autodiggers").textContent = data.numAutodiggers
+        document.getElementById("autodigger-cost").textContent = data.autodiggerCost
     });
 
     // Fetching the /updateStats route and updating the user's stats
