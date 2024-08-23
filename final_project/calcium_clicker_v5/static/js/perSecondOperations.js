4c169d4ba4b5e206c0dd7ebdae3ee45e62a42251
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-23 18:17:46 +0000

    Fri, Aug 23, 2024, 2:17 PM -04:00

--- final_project/calcium_clicker_v5/static/js/perSecondOperations.js
+++ final_project/calcium_clicker_v5/static/js/perSecondOperations.js
@@ -5,7 +5,7 @@ function perSecondOperations() {
     while still only updating the user's server-side skeleton count once per second to minimize
     strain on the server. */
     const interval = 50; // Update every 50ms (0.05 seconds)
-    let gainPerInterval; // Pre-initializing the gainPerInterval variable
+    let skeletonGainPerInterval; // Pre-initializing the skeletonGainPerInterval variable
 
     // Defining a function to fetch the per-second gain from the server.
     function fetchPerSecondGain() {
@@ -14,7 +14,7 @@ function perSecondOperations() {
         })
         .then(response => response.json())
         .then(data => {
-            gainPerInterval = data.skeletonsPerSecond / (1000 / interval);
+            skeletonGainPerInterval = data.skeletonsPerSecond / (1000 / interval);
         })
         // Catching any error in the process of fetching initial per-second gain from the server
         .catch(error => console.error("Error fetching per-second gain:", error));
@@ -28,7 +28,7 @@ function perSecondOperations() {
         let skeletonCountElement = document.getElementById("skeleton-count");
         let totalSkeletonsElement = document.getElementById("total-skeletons");
         let currentSkeletonCount = parseFloat(skeletonCount.textContent);
-        currentSkeletonCount += gainPerInterval;
+        currentSkeletonCount += skeletonGainPerInterval;
         // Rounding the textContent in the "skeleton-count" element down to ensure
         // that it remains a float.
         skeletonCount.textContent = Math.floor(currentSkeletonCount);
