f02fa7f12a3b09fa5c77a572ecb7bb422440e0e5
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-23 19:48:38 +0000

    Fri, Aug 23, 2024, 3:48 PM -04:00

---
+++ final_project/calcium_clicker_v5/static/js/refreshPage.js
@@ -0,0 +1,2 @@
+// The point of this script is to check when the user's totalSkeletons count reaches certain thresholds where new elements should be added to the page.
+// When these thresholds are reached, the script will refresh the page to allow the new elements to generate
