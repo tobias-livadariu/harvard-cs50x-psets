6593ae7bf6803ad48063c8ecf02ba7dc873c331c
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-24 21:37:22 +0000

    Sat, Aug 24, 2024, 5:37 PM -04:00

--- final_project/calcium_clicker_v6/static/js/digUpSkeletons.js
+++ final_project/calcium_clicker_v6/static/js/digUpSkeletons.js
@@ -4,8 +4,8 @@ function digUpSkeletons() {
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById("skeleton-count").textContent = data.skeletonCount;
-        document.getElementById("total-skeletons").textContent = data.totalSkeletons;
+        document.getElementById("skeleton-count").textContent = formatNumberSuffix(data.skeletonCount);
+        document.getElementById("total-skeletons").textContent = formatNumberSuffix(data.totalSkeletons);
 
         // Updating the key-value pairs in userSkeletons
         userSkeletons.skeletonCount = data.skeletonCount;
