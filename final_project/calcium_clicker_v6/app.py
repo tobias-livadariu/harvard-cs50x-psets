538580e1caeda8ec59884c5e396ad9cac0fb0dd7
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-25 03:31:04 +0000

    Sat, Aug 24, 2024, 11:31 PM -04:00

--- final_project/calcium_clicker_v6/app.py
+++ final_project/calcium_clicker_v6/app.py
@@ -118,15 +118,15 @@ def buyAutodigger():
     # Checking if numBuying, numAutodiggers, cost, and skeletonCount exist
     if numBuying and numAutodiggers and cost and skeletonCount:
         # Updating values in all pertinent variables
-        numAutodiggers += numBuying
         skeletonCount -= cost
+        numAutodiggers += numBuying
+        autodiggerCost = calculateAutodiggerCost(numAutodiggers=numAutodiggers, baseCost=10, growthRate=1.15)
 
         # Updating skeletonCount
         db.execute("UPDATE users SET skeletonCount = ? WHERE id = ?", skeletonCount, session["user_id"])
 
         # Updating numAutodiggers and autodiggerCost in one go
-        db.execute("UPDATE simple_upgrades SET numAutodiggers = ?, autodiggerCost = ? WHERE user_id = ?",
-                   numAutodiggers + numBuying, calculateAutodiggerCost(numAutodiggers=(numAutodiggers + numBuying), baseCost=10, growthRate=1.15), session["user_id"])
+        db.execute("UPDATE simple_upgrades SET numAutodiggers = ?, autodiggerCost = ? WHERE user_id = ?", numAutodiggers, autodiggerCost, session["user_id"])
     else:
         # Fetching autodiggerCost, numAutodiggers, and skeletonCount in one go
         userData = db.execute("""SELECT skeletonCount, autodiggerCost, numAutodiggers FROM users
