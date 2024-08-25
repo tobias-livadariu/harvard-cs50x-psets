87ef21aca62652cd99b130505a32e01859de8893
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-25 01:52:09 +0000

    Sat, Aug 24, 2024, 9:52 PM -04:00

--- final_project/calcium_clicker_v6/static/js/buyTenAutodiggers.js
+++ final_project/calcium_clicker_v6/static/js/buyTenAutodiggers.js
@@ -3,22 +3,17 @@ function buyTenAutodiggers() {
         method: "POST",
     })
     .then(response => response.json())
-    .then(data => {
+    .then(async data => {
         /* Getting the max number of autodiggers the user can buy. */
         let numAutodiggersBuyable = data.numAutodiggersBuyable
         if (numAutodiggersBuyable < 10) {
             throw new Error("10x transaction failed")
         }
 
-        /* Buying 10 autodiggers for the user sequentially.
-        Note that ChatGPT helped me write the syntax to make the
-        calls sequential, helping to avoid race conditions and
-        other issues.*/
-        let promises = [];
+        /* Buying 10 autodiggers for the user. */
         for (let i = 0; i < 10; i++) {
-            promises.push(buyAutodigger());
+            await buyAutodigger()
         }
-        return Promise.all(promises);  // Ensures all buyAutodigger() calls are completed
     })
     .catch(error => {
         console.error("Error during purchase:", error);
