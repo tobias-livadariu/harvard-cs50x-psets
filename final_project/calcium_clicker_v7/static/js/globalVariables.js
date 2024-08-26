13cac7a57cd099039da77a9afdbc648718a9037b
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-26 05:43:10 +0000

    Mon, Aug 26, 2024, 1:43 AM -04:00

--- final_project/calcium_clicker_v7/static/js/globalVariables.js
+++
@@ -1,7 +0,0 @@
-/* Defining a variable that can be used
-to keep track of the user's current and
-total skeleton counts. */
-let userSkeletons = {
-    skeletonCount: 0,
-    totalSkeletons: 0
-}
--- final_project/calcium_clicker_v7/static/js/shovelSkeletonsPerClick.js
+++
@@ -1,4 +0,0 @@
-// JavaScript function for determining the number of skeletons per click each shovel provides
-function shovelSkeletonsPerClick(curShovel, baseValue = 10, polyPower = 1.5, exponentialKicker = 1.02) {
-    return Math.floor(baseValue * Math.pow((curShovel + 1), polyPower) * Math.pow(exponentialKicker, Math.sqrt(curShovel + 1)));
-}
