b71c080773cec47dcfc737b72e3d64c4e8dd70f8
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-27 19:05:15 +0000

    Tue, Aug 27, 2024, 3:05 PM -04:00

--- final_project/calcium_clicker_v8/static/js/resyncDOM.js
+++ final_project/calcium_clicker_v8/static/js/resyncDOM.js
@@ -1,6 +1,13 @@
-// Defining a function that refreshes the values in the skeletonCount and totalSkeletons text boxes.
+// Defining two functions that refresh the values in the skeletonCount and totalSkeletons text boxes.
 // Note that this has to be done here rather than in the functions that actually change the values,
 // because too many functions are touching skeletonCount and totalSkeletons.
+
+skeletonCount = 0
+totalSkeletons = 0
+
+function loadValues() {
+    skeletonCount = 
+}
 
 function resyncDOM() {
     document.getElementById("skeleton-count").textContent = formatNumberSuffix(userSkeletons.skeletonCount)
