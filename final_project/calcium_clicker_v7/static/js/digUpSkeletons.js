70fd29a2f3365a37c7c1d7123af841a0ffc45645
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-27 00:20:16 +0000

    Mon, Aug 26, 2024, 8:20 PM -04:00

--- final_project/calcium_clicker_v7/static/js/digUpSkeletons.js
+++ final_project/calcium_clicker_v7/static/js/digUpSkeletons.js
@@ -5,8 +5,9 @@ function digUpSkeletons() {
     // Checking if currently digging skeletons
     if (!canDigUpSkeletons) {
         // Throw an error if canDigUpSkeletons is false to prevent the function from running further
-        throw new Error("Cannot buy max autodiggers at the moment.");
+        throw new Error("Cannot dig more skeletons at the moment.");
     }
+    canDigUpSkeletons = false;
 
     fetch("digUpSkeletons", {
         method: "POST",
@@ -19,5 +20,13 @@ function digUpSkeletons() {
         // Updating the key-value pairs in userSkeletons
         userSkeletons.skeletonCount = data.skeletonCount;
         userSkeletons.totalSkeletons = data.totalSkeletons;
+    })
+    .finally(() => {
+        canDigUpSkeletons = true
+    })
+    // Handling errors
+    .catch(error => {
+        console.error("Error during dig:", error);
+        canBuyMaxAutodiggers = true; // Reset the flag even if there is an error
     });
 }
