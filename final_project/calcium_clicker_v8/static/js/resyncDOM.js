8b7186cdb1d51e3cff2aa383a3a531b5acad5345
Tobias Livadariu <76631595+tobias-livadariu@users.noreply.github.com>
2024-08-27 19:10:08 +0000

    Tue, Aug 27, 2024, 3:10 PM -04:00

--- final_project/calcium_clicker_v8/static/js/resyncDOM.js
+++ final_project/calcium_clicker_v8/static/js/resyncDOM.js
@@ -2,18 +2,11 @@
 // Note that this has to be done here rather than in the functions that actually change the values,
 // because too many functions are touching skeletonCount and totalSkeletons.
 
-skeletonCount = 0
-totalSkeletons = 0
-
-function loadValues() {
-    skeletonCount = 
-}
-
 function resyncDOM() {
     document.getElementById("skeleton-count").textContent = formatNumberSuffix(userSkeletons.skeletonCount)
     document.getElementById("total-skeletons").textContent = formatNumberSuffix(userSkeletons.totalSkeletons)
 }
 
-document.getEventHandler("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     setInterval(resyncDOM, 25)
 })
