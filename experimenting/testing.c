0b60c91c134578af596bbf0f4b165ab9e1cb5988
Tobias Livadariu <tobias.livadariu@gmail.com>
2024-08-30 22:20:38 -0400

    removed experimenting folder

--- experimenting/testing.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    char* strArray[] = {"string1", "string2", "string3"};
-    printf("%s\n", strArray[1]);
-}
--- experimenting/week1_c/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    string first = get_string("What is your first name? ");
-    string last = get_string("What is your last name? ");
-    printf("Hello, %s %s\n", first, last);
-}
